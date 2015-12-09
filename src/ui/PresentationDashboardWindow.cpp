b4dba273179fe6e728c2f9eb8f80e2e46aa7bf23
Stephen <stephen@nowhere>
2015-12-09 13:13:57 -0500

    Added rows past member name: presentations

--- src/ui/PresentationDashboardWindow.cpp
+++ src/ui/PresentationDashboardWindow.cpp
@@ -13,6 +13,8 @@
 #include "UIUtils.h"
 #include "VisualizationWindow.h"
 
+static const int totalNumColumn = 4;
+
 PresentationDashboardWindow::PresentationDashboardWindow(const QString &csv_filename) {
 	PresentationParser parser;
 	
@@ -30,7 +32,7 @@ PresentationDashboardWindow::PresentationDashboardWindow(const QString &csv_file
 	}
 	
 	ui.treeWidget->setHeaderLabels(QStringList() << 
-						"" << "Presentation Type" << "Faculty Name" << "Total #" << "");
+						"" << "Presentation Type" << "Faculty Name" << "Title" << "Total #" << "");
 	
 	ui.titleLabel->setText("Presentations Summary, Department of " + records[0].primaryDomain);
 	ui.statusbar->showMessage("Read " + QString::number(records.size()) + " records from " + csv_filename);
@@ -55,29 +57,39 @@ void PresentationDashboardWindow::updateTreeWidget() {
 	//count the records
 	QMap<QString, int> presTypeSummary;
 	QMap<QString, QMap<QString, int>> nameSummary;
+	QMap<QString, QMap<QString, QSet<QString>>> titleSummary;
 	
 	for (const PresentationRecord &record : recordsInRange) {
         ++presTypeSummary[record.type];
 		
         ++nameSummary[record.type][record.memberName];
+		
+		titleSummary[record.type][record.memberName].insert(record.title);
 	}
 	
 	//build the view
     QTreeWidgetItem *root = new QTreeWidgetItem(ui.treeWidget, (QStringList() << 
-									"Presentations" << "" << "" << QString::number(recordsInRange.size())));
-	root->setTextAlignment(3, Qt::AlignRight); //"Total #" column
+									"Presentations" << "" << "" << "" << QString::number(recordsInRange.size())));
+	root->setTextAlignment(totalNumColumn, Qt::AlignRight);
 	ui.treeWidget->expandItem(root);
 	
 	for (auto presType = presTypeSummary.begin(); presType != presTypeSummary.end(); ++presType) {
         QTreeWidgetItem *presNode = new QTreeWidgetItem(root, (QStringList() << 
-											"" << presType.key() << "" << QString::number(presType.value())));
-		presNode->setTextAlignment(3, Qt::AlignRight); //"Total #" column
+											"" << presType.key() << "" << "" << QString::number(presType.value())));
+		presNode->setTextAlignment(totalNumColumn, Qt::AlignRight);
 		
 		QMap<QString, int> &currNameSummary = nameSummary[presType.key()];
 		for (auto name = currNameSummary.begin(); name != currNameSummary.end(); ++name) {
             QTreeWidgetItem *nameNode = new QTreeWidgetItem(presNode, (QStringList() << 
-												"" << "" << name.key() << QString::number(name.value())));
-			nameNode->setTextAlignment(3, Qt::AlignRight); //"Total #" column
+												"" << "" << name.key() << "" << QString::number(name.value())));
+			nameNode->setTextAlignment(totalNumColumn, Qt::AlignRight);
+			
+			QSet<QString> &currTitleSummary = titleSummary[presType.key()][name.key()];
+			for (QString &title : sortSet(currTitleSummary)) {
+				QTreeWidgetItem *titleNode = new QTreeWidgetItem(nameNode, (QStringList() <<
+												"" << "" << "" << title << ""));
+				titleNode->setTextAlignment(totalNumColumn, Qt::AlignRight);
+			}
 		}
 	}
 
